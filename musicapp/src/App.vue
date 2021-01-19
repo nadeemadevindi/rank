8cef156c5ae63c924f81ffe93c8485c864628ed7
nadeemadevindi <nadeemadevindi96@gmail.com>
2021-01-19 09:13:02 +0530

    m project

---
+++ musicapp/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+
+     <form>
+    <div class="form-group">
+        <label for="exampleInputEmail1">Product Name</label>
+      <input type="text"
+      v-model="name" 
+      class="form-control" 
+      id="exampleInputEmail1" 
+      placeholder="Product Name"
+      v-validate="'required'"
+      >
+       
+   
+   </div>
+  </form>
+  
+  </div>
+
+  
+</template>
+
+<script>
+export default {
+ 
+}
+</script>
+
+<style>
+
+</style>
---
+++ musicapp/src/main.js
@@ -0,0 +1,12 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+
+import * as VeeValidate from 'vee-validate';
+Vue.use(VeeValidate);
+
+new Vue({
+  el: '#app',
+  render: h => h(App)
+  
+})
